d14049e7dfb8b5578faaefc869c4e4cf10098ce3
lykaencinas <lykaencinas222@gmail.com>
2018-10-25 22:32:48 +0800

    Exercise no.1

---
+++ ENCINAS_E1.py
@@ -0,0 +1,12 @@
+
+print ("hello word")
+print ("My name {}."
+.format("lyka"))
+print ("My spirit animal is {}  ."
+.format("dogs"))
+print ("because {}."
+.format("dog are the most lovely animals"))
+print ("When not in school, I love to {}."
+.format("watch vlogs"))
+print ("I dream of being a {} in the future.."
+.format("Business woman"))
