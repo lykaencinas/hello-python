897b7a51b70406b777eafc8fcefbca606dc8273f
lykaencinas <lykaencinas222@gmail.com>
2018-10-25 22:26:26 +0800

    Exercise no.1

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
+
 print ("hello word")
-print ("My name {}.i am {}y/o."
-.format("rechelle","16"))
+print ("My name {}."
+.format("lyka"))
+print ("My spirit animal is {}  ."
+.format("dogs"))
+print ("because {}."
+.format("dog are the most lovely animals"))
+print ("When not in school, I love to {}."
+.format("watch vlogs"))
+print ("I dream of being a {} in the future.."
+.format("Business woman"))
+
+
